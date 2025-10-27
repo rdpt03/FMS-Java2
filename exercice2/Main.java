725ab9d8b46ffe63b4d679988cc2b602096977a7
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-10-27 15:29:52 +0100

    exercice 2.4

--- exercice2/Main.java
+++ exercice2/Main.java
@@ -1,35 +1,44 @@
 package exercice2;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args){
-
-		
-		Scanner s = new Scanner(System.in);
-		
-		
-		System.out.println("Inserer un numero: ");
-		if(s.hasNextInt()){ //solution pour question 2.3
-			int number = s.nextInt();
-			//check positive or negative
-			if(number > 0){
-				System.out.println(number+" est positif");
+		ArrayList<Integer> numbers = new ArrayList<>();
+		if(args.length > 0){
+			for(String argument : args){
+				if(stringToInt(argument)){
+					numbers.add(Integer.parseInt(argument));
+				}
 			}
-			else if(number < 0){
-				System.out.println(number+" est negatif");
+		}else{
+			Scanner s = new Scanner(System.in);
+			System.out.println("Inserer un numero: ");
+			if(s.hasNextInt()){
+				numbers.add(s.nextInt());
 			}
-			else{
-				System.out.println("est 0");
-			}
+		}	
 			
-			//check even or odd
-			if(number % 2 == 0){
-				System.out.println(number+" est pair");
+		if(numbers.size() > 0){ //solution pour question 2.3
+			for(int number : numbers){
+				//check positive or negative
+				if(number > 0){
+					System.out.println(number+" est positif");
+				}
+				else if(number < 0){
+					System.out.println(number+" est negatif");
+				}
+				else{
+					System.out.println("est 0");
+				}
+				
+				//check even or odd
+				if(number % 2 == 0){
+					System.out.println(number+" est pair");
+				}
+				else{
+					System.out.println(number+" est impair");
+				}
 			}
-			else{
-				System.out.println(number+" est impair");
-			}
-			
 		}
 		else{
 			System.out.println("n'est pas un numero");
